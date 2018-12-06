2c05dd2f18b00b506e7410138732f0fb113d8ada
madijanz <42851602+madijanz@users.noreply.github.com>
2018-12-06 16:44:34 -0500

    added final files

---
+++ final/js/main.js
@@ -0,0 +1,17 @@
+var pElement = document.getElementById('p-tag'); 
+var button = document.getElementsByClassName('Btn');
+
+
+
+function popUp() {
+	alert("Subscribe to teenVOGUE")
+}
+
+function changeCSS() {
+pElement.style.color = "red";
+pElement.style.fontSize = "15px";
+pElement.style.textAlign = "center";
+
+}
+
+
